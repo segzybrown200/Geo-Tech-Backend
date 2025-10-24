30c7dedd8004a5375f58861b3b00b539ebaeabbf
segzybrown200 <segzybrown2003@gmail.com>
2025-10-24 03:56:56 +0100

    make ooooo

--- src/router/cofoRoutes.ts
+++ src/router/cofoRoutes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import multer from 'multer';
 import { applyForCofO, reviewCofO } from '../controllers/cofoController';
+import { verifyToken } from '../middlewares/authMiddleware';
+import { initializePayment, verifyPayment } from '../controllers/paymentController';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/apply', upload.array('documents'), applyForCofO);
 router.post('/review/:id', reviewCofO); // :id = CofOApplication.id
-
+router.post("/init", verifyToken, initializePayment);
+router.get("/verify", verifyPayment);
 export default router;
--- src/router/ownershipRoutes.ts
+++ src/router/ownershipRoutes.ts
@@ -1,11 +1,16 @@
 // src/routes/ownershipRoutes.ts
-import express from 'express';
-import { transferOwnership, getOwnershipHistory } from '../controllers/ownershipController';
-import { verifyToken } from '../middlewares/authMiddleware';
+import express from "express";
+import { verifyToken } from "../middlewares/authMiddleware";
+import {
+  initiateTransfer,
+  verifyTransferCode,
+  finalizeTransfer,
+} from "../controllers/ownershipController";
 
 const router = express.Router();
 
-router.post('/transfer', verifyToken, transferOwnership);
-router.get('/history/:landId', verifyToken, getOwnershipHistory);
+router.post("/initiate", verifyToken, initiateTransfer);
+router.post("/verify", verifyToken, verifyTransferCode);
+router.post("/finalize", verifyToken, finalizeTransfer);
 
 export default router;
