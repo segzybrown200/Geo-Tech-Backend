c5cef50788bb30ad302973d163d16d9db0393f39
segzybrown200 <segzybrown2003@gmail.com>
2025-10-24 04:12:16 +0100

    make ooooo

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from '@prisma/client';;
 import {
   registerSchema,
   requestPasswordResetSchema,
