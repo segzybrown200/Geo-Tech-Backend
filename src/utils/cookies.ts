19cc28e243d8215a5c9aa8afbd616a2e860ea250
SegCode200 <ajayisegun2003@gmail.com>
2026-01-11 22:21:46 +0100

    make he just work na

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,14 +1,10 @@
 import { Response } from "express";
 
-// const isProd = process.env.NODE_ENV === "production";
 export const setSessionCookie = (res: Response, token: string) => {
   res.cookie("geo_session", token, {
     httpOnly: true,
-    // secure: false,
-    sameSite: "none",
+    sameSite: "lax",
     secure: false,   
-    // secure: isProd, // ❗ false on localhost
-    // sameSite: isProd ? "strict" : "lax",
     maxAge: 24 * 60 * 60 * 1000, // 24 hrs
     path: "/",
   });
@@ -18,9 +14,7 @@ export const clearSessionCookie = (res: Response) => {
   res.clearCookie("geo_session", {
     httpOnly: true,
     secure: false,
-    sameSite: "none",
+    sameSite: "lax",
     path: "/",
-    // secure: isProd,
-    // sameSite: isProd ? "strict" : "lax",
   });
 };
