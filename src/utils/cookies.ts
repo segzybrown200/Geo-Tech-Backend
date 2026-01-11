6be2717ba70d869451f6308ac3cd4394472bf8b0
SegCode200 <ajayisegun2003@gmail.com>
2026-01-11 21:23:14 +0100

    test ooo

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -4,8 +4,8 @@ import { Response } from "express";
 export const setSessionCookie = (res: Response, token: string) => {
   res.cookie("geo_session", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    // secure: false,
+    sameSite: "none",
     // secure: isProd, // ❗ false on localhost
     // sameSite: isProd ? "strict" : "lax",
     maxAge: 24 * 60 * 60 * 1000, // 24 hrs
@@ -17,7 +17,7 @@ export const clearSessionCookie = (res: Response) => {
   res.clearCookie("geo_session", {
     httpOnly: true,
     secure: false,
-    sameSite: "lax",
+    sameSite: "none",
     path: "/",
     // secure: isProd,
     // sameSite: isProd ? "strict" : "lax",
