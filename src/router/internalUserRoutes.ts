79f924593baf171dbfe0a56a8be5f35034d19e23
SegCode200 <ajayisegun2003@gmail.com>
2026-01-14 20:16:06 +0100

    make he just work oo

--- src/router/internalUserRoutes.ts
+++ src/router/internalUserRoutes.ts
@@ -1,21 +1,21 @@
 import express from 'express';
 import { createInternalUser, getInternalUserSession, loginInternalUser, refreshInternalToken, resendInternalVerification, setInternalUserPassword, updateSignature, uploadSignature, verifyInternalEmail } from '../controllers/internalUserController';
-import { internalUserAuth, verifyToken } from '../middlewares/authMiddleware';
+import { internalUserAuth, AdminverifyToken } from '../middlewares/authMiddleware';
 import { authorizeRoles } from '../middlewares/roleMiddleware';
 import multer from 'multer';
 const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
-router.post('/', verifyToken, authorizeRoles(['ADMIN']), createInternalUser);
-router.post('/upload-signature', verifyToken, authorizeRoles(['GOVERNOR']), upload.single('signature'), uploadSignature);
-router.patch('/update-signature', verifyToken, authorizeRoles(['GOVERNOR']), upload.single('signature'), updateSignature);
+router.post('/', AdminverifyToken, authorizeRoles(['ADMIN']), createInternalUser);
+router.post('/upload-signature', AdminverifyToken, authorizeRoles(['GOVERNOR']), upload.single('signature'), uploadSignature);
+router.patch('/update-signature', AdminverifyToken, authorizeRoles(['GOVERNOR']), upload.single('signature'), updateSignature);
 router.get('/verify', verifyInternalEmail);
 router.post('/set-password', setInternalUserPassword);
 router.post('/resend-verification', resendInternalVerification);
 router.post("/login", loginInternalUser);
 router.get("/session", internalUserAuth, getInternalUserSession);
-router.get("/refresh", verifyToken, refreshInternalToken);
+router.get("/refresh", AdminverifyToken, refreshInternalToken);
 
 
 export default router;
