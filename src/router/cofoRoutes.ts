7d987d02bd70ff72c9c229cd449f7cb46cd9f36a
SegCode200 <ajayisegun2003@gmail.com>
2026-01-24 08:17:56 +0100

    more added

--- src/router/cofoRoutes.ts
+++ src/router/cofoRoutes.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import multer from 'multer';
 import { applyForCofO, getCofOById, resubmitCofO, reviewCofO, getMyCofOApplications } from '../controllers/cofoController';
-import { requireAuth, verifyToken } from '../middlewares/authMiddleware';
+import { requireAuth, verifyToken, internalUserAuth } from '../middlewares/authMiddleware';
 import { initializePayment, verifyPayment } from '../controllers/paymentController';
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post('/apply/:cofOApplicationId', upload.array('documents'), requireAuth, applyForCofO);
-router.post('/review/:id', reviewCofO); // :id = CofOApplication.id
+router.post('/review/:id',internalUserAuth, reviewCofO); // :id = CofOApplication.id
 router.post("/init", verifyToken, initializePayment);
 router.get("/get-applications/:cofOId", requireAuth, getCofOById )
 router.get("/verify", verifyPayment);
